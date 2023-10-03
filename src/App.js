f973b3e67342c32e3f78893cfddf14bf76a327e4
Suri K <suren8990@gmail.com>
2023-10-02 21:01:54 -0400

    add more code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
-import purepints from './purepints.png'
-import React from 'react';
+import {
+  BrowserRouter as Router, 
+  Switch,
+  Route
+} from 'react-router-dom'
+import Layout from './layout/Layout'
+import Home from './components/pages/Home'
+import AboutUs from './components/pages/AboutUs'
+import ContactUs from './components/pages/ContactUs'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={purepints} className="App-logo" alt="purepints" />
-        <h1>Hello from V2</h1>
-      </header>
+    <div>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path={'/AboutUs'} component={AboutUs}></Route>
+            <Route path={'/ContactUs'} component={ContactUs}></Route>
+            <Route path={'/'} component={Home}></Route>
+          </Switch>
+        </Layout>
+      </Router>
     </div>
   );
 }
